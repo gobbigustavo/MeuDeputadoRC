aec4c71794f8b3587861ddfcefbb1a153ac9d4d5
Filipe Falcão <filipefalcaosantos@gmail.com>
2017-05-22 23:04:24 -0300

    Solve init errors

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -1,122 +1,91 @@
 var app = {
-  initialize: function () {
-    this.bindEvents();
-  },
+    initialize: function () {
+        this.bindEvents();
+    },
 
-  bindEvents: function () {
-    document.addEventListener('deviceready', this.onDeviceReady, false);
-  },
+    bindEvents: function () {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
 
-  onDeviceReady: function () {
-  }
+    onDeviceReady: function () {
+    }
 };
 
 app.initialize();
 
 angular.module('App', ['ngMaterial', 'ngRoute', 'firebase'])
-    
-  // .run(["$rootScope", "$location", function($rootScope, $location) {
-  //     $rootScope.$on("$routeChangeError", function(event, next, previous, error) {
-  //       // We can catch the error thrown when the $requireAuth promise is rejected
-  //       // and redirect the user back to the home page
-  //       if (error === "AUTH_REQUIRED") {
-  //           $location.path("/home");
-  //       }
-  //     });
-  // }])
-  //
-  // .factory("Auth", ["$firebaseAuth", function($firebaseAuth) {
-  //   var ref = new Firebase("https://scorching-fire-8060.firebaseio.com");
-  //   return $firebaseAuth(ref);
-  // }])
 
-    .controller("SampleCtrl", function($scope, $firebaseObject) {
-      var ref = firebase.database().ref();
-      // download the data into a local object
-      $scope.data = $firebaseObject(ref);
-      // putting a console.log here won't work, see below
+    .controller("SampleCtrl", function ($scope, $firebaseObject) {
+        var ref = firebase.database().ref();
+        // download the data into a local object
+        $scope.data = $firebaseObject(ref);
+        // putting a console.log here won't work, see below
+    })
+
+    .service('sharedObj', function () {
+        var obj = {};
+
+        var setObj = function (item) {
+            obj = item;
+        };
+
+        var getObj = function () {
+            return obj;
+        };
+
+        return {
+            setObj: setObj,
+            getObj: getObj
+        }
     })
 
 
-
-  .service('sharedObj', function () {
-    var obj = {};
-
-    var setObj = function(item) {
-      obj = item;
-    };
-
-    var getObj = function() {
-      return obj;
-     };
-
-    return {
-      setObj: setObj,
-      getObj: getObj
-    }
-  })
-
-
-  .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.
-      when('/login', {
-        templateUrl: 'views/login.html',
-        controller: 'SampleCtrl'
-        // resolve: {
-        //     // controller will not be loaded until $requireAuth resolves
-        //     // Auth refers to our $firebaseAuth wrapper in the example above
-        //     "currentAuth": ["Auth", function(Auth) {
-        //         // $requireAuth returns a promise so the resolve waits for it to complete
-        //         // If the promise is rejected, it will throw a $stateChangeError (see above)
-        //         return Auth.$requireAuth();
-        //     }]
-        // }
-      }).
-      
-      when('/register', {
-        templateUrl: 'views/registration.html',
-        controller: 'SampleCtrlr'
-      }).
-
-      when('/home', {
-        templateUrl: 'views/main.html',
-        controller: 'SampleCtrl'
-        // resolve: {
-        //     // controller will not be loaded until $waitForAuth resolves
-        //     // Auth refers to our $firebaseAuth wrapper in the example above
-        //     "currentAuth": ["Auth", function(Auth) {
-        //         // $waitForAuth returns a promise so the resolve waits for it to complete
-        //         return Auth.$waitForAuth();
-        //     }]
-        //}
-      }).
-      when('/drugstore', {
-        templateUrl: 'views/drugstore.html',
-        controller: 'SampleCtrl'
-      }).
-    when('/projetosdelei', {
-        templateUrl: 'views/projetosdelei.html',
-        controller: 'SampleCtrl'
-      }).
-    when('/rodrigo', {
-        templateUrl: 'views/rodrigo.html',
-        controller: 'SampleCtrl'
-      }).
-    when('/votacaonaale', {
-        templateUrl: 'views/votacaonaale.html',
-        controller: 'SampleCtrl'
-      }).
-    when('/leinaale', {
-        templateUrl: 'views/leinaale.html',
-        controller: 'SampleCtrl'
-      }).
-    
-     when('/boaspraticas', {
-        templateUrl: 'views/boaspraticas.html',
-        controller: 'SampleCtrl'
-      }).
-
-      otherwise({
-        redirectTo: '/login'
-      });
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/login', {
+            templateUrl: 'views/login.html',
+            controller: 'SampleCtrl'
+            // resolve: {
+            //     // controller will not be loaded until $requireAuth resolves
+            //     // Auth refers to our $firebaseAuth wrapper in the example above
+            //     "currentAuth": ["Auth", function(Auth) {
+            //         // $requireAuth returns a promise so the resolve waits for it to complete
+            //         // If the promise is rejected, it will throw a $stateChangeError (see above)
+            //         return Auth.$requireAuth();
+            //     }]
+            // }
+        }).when('/register', {
+            templateUrl: 'views/registration.html',
+            controller: 'SampleCtrlr'
+        }).when('/home', {
+            templateUrl: 'views/main.html',
+            controller: 'SampleCtrl'
+            // resolve: {
+            //     // controller will not be loaded until $waitForAuth resolves
+            //     // Auth refers to our $firebaseAuth wrapper in the example above
+            //     "currentAuth": ["Auth", function(Auth) {
+            //         // $waitForAuth returns a promise so the resolve waits for it to complete
+            //         return Auth.$waitForAuth();
+            //     }]
+            //}
+        }).when('/drugstore', {
+            templateUrl: 'views/drugstore.html',
+            controller: 'SampleCtrl'
+        }).when('/projetosdelei', {
+            templateUrl: 'views/projetosdelei.html',
+            controller: 'SampleCtrl'
+        }).when('/rodrigo', {
+            templateUrl: 'views/rodrigo.html',
+            controller: 'SampleCtrl'
+        }).when('/votacaonaale', {
+            templateUrl: 'views/votacaonaale.html',
+            controller: 'SampleCtrl'
+        }).when('/leinaale', {
+            templateUrl: 'views/leinaale.html',
+            controller: 'SampleCtrl'
+        }).when('/boaspraticas', {
+            templateUrl: 'views/boaspraticas.html',
+            controller: 'SampleCtrl'
+        }).otherwise({
+            redirectTo: '/login'
+        });
     }]);
