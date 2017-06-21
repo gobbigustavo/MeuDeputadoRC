1915107609562b32b68d6cbe2714c6c82365f180
Bhumi Shah <test@youremail.com>
2017-06-21 19:33:22 -0300

    21/06/17

--- app/platforms/android/assets/www/js/index.js
+++ app/platforms/android/assets/www/js/index.js
@@ -14,17 +14,55 @@ var app = {
 app.initialize();
 
 angular.module('App', ['ngMaterial', 'ngRoute', 'firebase'])
+    
+    .controller("SampleCtrl", function ($scope, $firebaseObject, $firebaseArray, $mdDialog) {
+        var ref = firebase.database().ref().child("votacaonaale");
 
-    .controller("SampleCtrl", function ($scope, $firebaseObject) {
-        var ref = firebase.database().ref().child("votacaonaale").child("lei1").child("titulo");
+        // Option 1
+        var votacaonaale = $firebaseArray(ref);
+        votacaonaale.$loaded()
+            .then(function() {
+                $scope.list = votacaonaale;
+            })
+            .catch(function(error) {
+                console.log("Error:", error);
+            });
 
-        var syncObject = $firebaseObject(ref);
-        syncObject.$bindTo($scope, "votacaonaale");
+        // Option 2
+        var votacaonaale2 = $firebaseObject(ref);
+        votacaonaale2.$bindTo($scope, "list2");
 
-        
+        // Compute votes yes
+        $scope.computeVotesYes = function(index) {
+            $scope.list[index].sim += 1;
+            $scope.list.$save(index);
+        };
+
+        // Compute votes no
+        $scope.computeVotesNo = function(index) {
+            $scope.list[index].nao += 1;
+            $scope.list.$save(index);
+        };
+    
+        $scope.showAlert = function(ev) {
+            // Appending dialog to document.body to cover sidenav in docs app
+            // Modal dialogs should fully cover application
+            // to prevent interaction outside of dialog
+            $mdDialog.show(
+              $mdDialog.alert()
+                .parent(angular.element(document.querySelector('#popupContainer')))
+                .clickOutsideToClose(true)
+                .title('Seu voto foi computado com sucesso!')
+                .textContent('Obrigado por dar seu voto e contribuir com a democracia em Alagoas!')
+                .ariaLabel('Popup de Confirmação')
+                .ok('Ok')
+                .targetEvent(ev)
+            );
+          };
+ 
     })
 
-
+    
 
     .service('sharedObj', function () {
         var obj = {};
@@ -43,7 +81,6 @@ angular.module('App', ['ngMaterial', 'ngRoute', 'firebase'])
         }
     })
 
-
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/login', {
             templateUrl: 'views/login.html',
@@ -59,7 +96,7 @@ angular.module('App', ['ngMaterial', 'ngRoute', 'firebase'])
             // }
         }).when('/register', {
             templateUrl: 'views/registration.html',
-            controller: 'SampleCtrlr'
+            controller: 'SampleCtrl'
         }).when('/home', {
             templateUrl: 'views/main.html',
             controller: 'SampleCtrl'
@@ -89,7 +126,25 @@ angular.module('App', ['ngMaterial', 'ngRoute', 'firebase'])
         }).when('/boaspraticas', {
             templateUrl: 'views/boaspraticas.html',
             controller: 'SampleCtrl'
+        }).when('/deputadonapraca', {
+            templateUrl: 'views/deputadonapraca.html',
+            controller: 'SampleCtrl'
+        }).when('/deputadoporumdia', {
+            templateUrl: 'views/deputadoporumdia.html',
+            controller: 'SampleCtrl'
+         }).when('/revogai', {
+            templateUrl: 'views/revogai.html',
+            controller: 'SampleCtrl'
+        }).when('/politicadeverdade', {
+            templateUrl: 'views/politicadeverdade.html',
+            controller: 'SampleCtrl'
+            }).when('/selecaopublica', {
+            templateUrl: 'views/selecaopublica.html',
+            controller: 'SampleCtrl'
+        }).when('/suacidade', {
+            templateUrl: 'views/suacidade.html',
+            controller: 'SampleCtrl'    
         }).otherwise({
-            redirectTo: '/login'
+            redirectTo: '/home'
         });
     }]);
--- app/www/js/index.js
+++ app/www/js/index.js
@@ -14,8 +14,8 @@ var app = {
 app.initialize();
 
 angular.module('App', ['ngMaterial', 'ngRoute', 'firebase'])
-
-    .controller("SampleCtrl", function ($scope, $firebaseObject, $firebaseArray) {
+    
+    .controller("SampleCtrl", function ($scope, $firebaseObject, $firebaseArray, $mdDialog) {
         var ref = firebase.database().ref().child("votacaonaale");
 
         // Option 1
@@ -43,7 +43,26 @@ angular.module('App', ['ngMaterial', 'ngRoute', 'firebase'])
             $scope.list[index].nao += 1;
             $scope.list.$save(index);
         };
+    
+        $scope.showAlert = function(ev) {
+            // Appending dialog to document.body to cover sidenav in docs app
+            // Modal dialogs should fully cover application
+            // to prevent interaction outside of dialog
+            $mdDialog.show(
+              $mdDialog.alert()
+                .parent(angular.element(document.querySelector('#popupContainer')))
+                .clickOutsideToClose(true)
+                .title('Seu voto foi computado com sucesso!')
+                .textContent('Obrigado por dar seu voto e contribuir com a democracia em Alagoas!')
+                .ariaLabel('Popup de Confirmação')
+                .ok('Ok')
+                .targetEvent(ev)
+            );
+          };
+ 
     })
+
+    
 
     .service('sharedObj', function () {
         var obj = {};
@@ -110,6 +129,21 @@ angular.module('App', ['ngMaterial', 'ngRoute', 'firebase'])
         }).when('/deputadonapraca', {
             templateUrl: 'views/deputadonapraca.html',
             controller: 'SampleCtrl'
+        }).when('/deputadoporumdia', {
+            templateUrl: 'views/deputadoporumdia.html',
+            controller: 'SampleCtrl'
+         }).when('/revogai', {
+            templateUrl: 'views/revogai.html',
+            controller: 'SampleCtrl'
+        }).when('/politicadeverdade', {
+            templateUrl: 'views/politicadeverdade.html',
+            controller: 'SampleCtrl'
+            }).when('/selecaopublica', {
+            templateUrl: 'views/selecaopublica.html',
+            controller: 'SampleCtrl'
+        }).when('/suacidade', {
+            templateUrl: 'views/suacidade.html',
+            controller: 'SampleCtrl'    
         }).otherwise({
             redirectTo: '/home'
         });
